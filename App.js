29d4bed488bb2deefb1eb27432825145ae178fa5
raphaelnrk <raphael.pena@fadesp.org.br>
2022-11-30 15:39:47 -0300

    Continuação do projeto

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { StatusBar } from 'react-native';
+
+
+import Routes from './src/Routes';
 
 
 export default function App() {
   return (
     <>
-    
+      <StatusBar barStyle={"auto"} />
+      <Routes />
     </>
   );
 }
