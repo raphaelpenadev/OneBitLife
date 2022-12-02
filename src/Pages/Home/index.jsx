cdf1946a2f79fe95ffbbc535587a555ed3a14f50
raphaelnrk <raphael.pena@fadesp.org.br>
2022-12-02 13:21:22 -0300

    Botão de adição de meta criado

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,10 +1,17 @@
+import React, { useState } from "react";
+
 import { useNavigation } from "@react-navigation/native";
 import { ScrollView, StyleSheet, Text, View } from "react-native";
 import LifeStatus from "../../Components/Common/LifeStatus";
+import CreateHabit from "../../Components/Home/CreateHabit";
 import StatusBar from "../../Components/Home/StatusBar";
 
 export default function Home() {
   const navigation = useNavigation();
+  const [mindHabit, setMindHabit] = useState();
+  const [moneyHabit, setMoneyHabit] = useState();
+  const [bodyHabit, setBodyHabit] = useState();
+  const [funHabit, setFunHabit] = useState();
 
   function handleNavExplanation() {
     navigation.navigate("AppExplanation");
@@ -17,6 +24,7 @@ export default function Home() {
           <Text style={styles.dailyChecks}>❤️ 20 dias - ✔️ 80 checks</Text>
           <LifeStatus />
           <StatusBar />
+          <CreateHabit habitArea="Mente" borderColor="#90B7F3" />
         </View>
         <Text style={styles.explanation}
           onPress={() => {
